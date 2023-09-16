544f62d9a4205b2d76faf7dfc4d2fc72b47640d3
unknown <vidaesviazalondres@gmail.com>
2023-09-15 18:27:23 -0600

    popo

---
+++ phytyon/Transformaciones.py
@@ -0,0 +1,27 @@
+#Transformacion 
+
+name = "nicolas"
+print(type(name))
+name = 12
+print (type(name))
+name = True
+print(type(name))
+
+
+print ("nicolas " + " Molina ")
+print ( 10 + 20 )
+
+print (" nicolas "+ " tiene " + " 12 " )
+
+age = 12
+
+print("mi edad es " + str (age))
+print(f"mi edad es {age}")
+
+age = input ('Escribe tu edad =>')
+print(type(age))
+age = int (age)
+age += 10
+print(f'Tu edad en 10 años será {age}')
+
+#La "f" significa formand = formato
---
+++ phytyon/operadores aritmeticos.py
@@ -0,0 +1,23 @@
+print (10 + 10)
+print (10 - 5)
+print (10 * 2)
+print (10 / 2) # 5
+print (10 % 2) # 0
+print (10 % 3)
+print (10 // 3)
+print (10 ** 3)
+print (2 ** 3)
+print (2 ** 3 + 3 - 7 / 1 // 4 )
+print ((7 / 1) // 4)
+print (10/3)
+
+
+# Exponencciacion **
+# Te deja los decimales y enteros al momento de dividir "/"
+# Lo divide entero y omite los punto decimal"//"
+# El porsentaje solo te deja el reciduo, ose lo que sombro "%"
+
+print(' hola ' + ' mundo' )
+
+print ('hola ' * 3)
+
